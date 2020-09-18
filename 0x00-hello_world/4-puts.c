6d7ae7027a2533552a1c483db78363727b91f19b
Jeff-28 <2408@holbertonschool.com>
2020-09-18 17:55:32 +0000

    Fixed the text to be printed.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 char main(void)
 
 {
-char *print = "Programming is like building a multilingual puzzle";
+char *print = "\"Programming is like building a multilingual puzzle";
 
 puts(print);
 return (0);
