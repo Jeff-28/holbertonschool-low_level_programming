dc486f70af423c4102838bc537ea90a8422ce47a
Jeff-28 <2408@holbertonschool.com>
2020-09-18 20:50:01 +0000

    Deleted 'The' from the text.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,15 +13,15 @@ long int longtype;
 char chartype;
 long long int longinttype;
 
-printf("The size of a char: %ld byte(s)\n", sizeof(chartype));
+printf("Size of a char: %ld byte(s)\n", sizeof(chartype));
 
-printf("The size of an int: %ld byte(s)\n", sizeof(inttype));
+printf("Size of an int: %ld byte(s)\n", sizeof(inttype));
 
-printf("The size of a long int: %ld byte(s)\n", sizeof(longtype));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longtype));
 
-printf("The size of a long long int: %ld byte(s)\n", sizeof(longinttype));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longinttype));
 
-printf("The size of a float: %ld byte(s)\n", sizeof(floattype));
+printf("Size of a float: %ld byte(s)\n", sizeof(floattype));
 
 return (0);
 }
