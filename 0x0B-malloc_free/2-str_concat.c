6718836047fcdb31ad729e44d2abf58dc086590f
Jeff Martinez <2408@holbertonschool.com>
2020-10-15 21:41:34 +0000

    Fixed amount of memory allocated and a check for malloc's success.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,50 @@
 #include <stdlib.h>
 #include "holberton.h"
-
 /**
  * str_concat - concatenates two strings.
  * @s1: destination string.
  * @s2: source string.
  * Return: pointer to new string.
  */
-
 char *str_concat(char *s1, char *s2)
 {
-
 char *str, *temp;
 int i = 0, j = 0;
-
+if (s1 == NULL)
+{
+s1 = NULL;
+}
+if (s2 == NULL)
+{
+s2 = NULL;
+}
 while (*(s1 + i) != '\0')
 {
 i++;
 }
-
 while (*(s2 + j) != '\0')
 {
 j++;
 }
-
-str = malloc(sizeof(char) * i + j);
-
+str = malloc(sizeof(char) * i + j + 1);
+if (str == NULL)
+{
+free(str);
+return (NULL);
+}
 temp = str;
-
 while (*s1)
 {
 *temp = *s1;
 temp++;
 s1++;
 }
-
 while (*s2)
 {
 *temp = *s2;
 temp++;
 s2++;
 }
-
 return (str);
 }
 
