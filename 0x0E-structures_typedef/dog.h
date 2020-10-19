70a51c6bf1a46b57450bcc48ebbab32319f25f70
Jeff Martinez <2408@holbertonschool.com>
2020-10-19 21:51:24 +0000

    Starting the project with a README and header file. The latter defines a new data type: struct dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - data struct for a dog's identification.
+ * @name: denotes the dog's name.
+ * @age: denotes the dog's age.
+ * @owner: denotes the dog's owner.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
