7dbef8480e35711ed3189e8105bc41c9fd078e38
Jeff Martinez <2408@holbertonschool.com>
2020-10-20 00:35:11 +0000

    Updated the header dog.h with new prototype. Adding a fuction that prints a struct dog.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog with its different elements.
+ * @d: pointer to the struct to be printed.
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+
+if (d == NULL)
+{
+printf(" ");
+}
+
+if (d->name == NULL)
+{
+d->name = "(nil)";
+}
+
+if (d->owner == NULL)
+{
+d->owner = "(nil)";
+}
+
+printf("Name: %s\n", d->name);
+
+if (d->age == 0)
+{
+printf("Age: (nil)\n");
+}
+else
+{
+printf("Age: %f\n", d->age);
+}
+
+printf("Owner: %s\n", d->owner);
+
+}
