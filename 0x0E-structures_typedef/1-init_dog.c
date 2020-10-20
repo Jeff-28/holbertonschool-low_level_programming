8a94b6318afeea98449498715f9b5220ab73daba
Jeff Martinez <2408@holbertonschool.com>
2020-10-20 02:12:21 +0000

    Eliminated the malloc function.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,14 +12,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-d = malloc(sizeof(struct dog));
-
-if (d == NULL)
-{
-free(d);
-exit(EXIT_FAILURE);
-}
-
 d->name = name;
 d->age = age;
 d->owner = owner;
