697a021d7aba5beed9407b26ba6a8178bbc48d29
Jeff Martinez <2408@holbertonschool.com>
2020-10-21 14:42:09 +0000

    Starting project 0x0F about function pointers, with a README and header file. Also the first task, which is a function to print a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: name of the person.
+ * @f: pointer to the function that prints.
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+(*f)(name);
+
+}
