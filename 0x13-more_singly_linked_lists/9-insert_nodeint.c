ae884331c19299a5bc13e805110058655eab8d1b
Jeff Martinez <2408@holbertonschool.com>
2020-11-03 20:38:46 +0000

    Fixed code to check if head is NULL.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *temp = (*head);
 	unsigned int pos = 1;
 
-	if (idx == 0)
+	if (idx == 0 || *head == NULL)
 	{
 		return (NULL);
 	}
