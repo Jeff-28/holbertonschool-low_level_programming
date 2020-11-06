454e65b510bd0816aa748ddef1c38f1149cd0ef1
Jeff Martinez <2408@holbertonschool.com>
2020-11-06 16:48:16 +0000

    Fixed indentations.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 *main - prints the last digit of the number stored in the variable.
 *Return: value of 0.
@@ -8,33 +9,33 @@
 
 int main(void)
 {
-int n;
-int l;
+	int n;
+	int l;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-
-l = n % 10;
-
-if (l == 0)
-{
-printf("Last digit of %d is 0 and is 0\n", n);
-
-}
-
-if (l > 5)
-{
-printf("Last digit of %d is %d and is grater than 5\n", n, l);
-}
+	srand(time(0));
+	n = 89;  /*rand() - RAND_MAX / 2;*/
 
 
+	l = n % 10;
 
-if (l < 6 && l != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	if (l == 0)
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
 
-}
+	}
 
-return (0);
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is grater than 5\n", n, l);
+	}
+
+
+
+	if (l != 0 && l < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+
+	}
+
+	return (0);
 }
