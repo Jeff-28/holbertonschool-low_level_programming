0a22ac0bf8607c5701f7554ef1b5b7e0e961af10
Jeff Martinez <2408@holbertonschool.com>
2020-11-06 15:33:50 +0000

    Adding a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,18 @@
 void print_binary(unsigned long int n)
 {
 
-	if (n > 1)
+	if (n == 0)
 	{
-		print_binary(n >> 1);
+		_putchar('0');
 	}
+	else
+	{
+		if ((n >> 1) != 0)
+		{
+			print_binary(n >> 1);
+		}
 
-	printf("%lu", n & 1);
+		_putchar((n & 1) + '0');
+	}
 
 }
