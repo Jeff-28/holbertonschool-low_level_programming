b388dc83265015abb7e98223ee5add727999e88e
Jeff Martinez <2408@holbertonschool.com>
2020-11-10 15:07:11 +0000

    Added an if statement to check for parameter text_content.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,18 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	while (text_content[len] != '\0')
+	if (text_content)
 	{
-		len++;
-	}
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
 
-	k = write(fd, text_content, len);
-	if (k == -1)
-	{
-		return (-1);
+		k = write(fd, text_content, len);
+		if (k == -1)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
