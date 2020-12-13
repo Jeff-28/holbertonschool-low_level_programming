685c42c9e66de8d837b17a0bf3ee55d0ab10b500
Jeff Martinez <2408@holbertonschool.com>
2020-12-13 16:55:40 +0000

    Fixed code for adding next data.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,10 +20,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = (*head);
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	new_node->next = *head;
 	new_node->prev = NULL;
-	(*head)->prev = new_node;
-	(*head) = new_node;
+
+	*head = new_node;
 
 	return (new_node);
 }
