4628845242dd9b0840083ce22878a8d9f4bcf54a
Jeff Martinez <2408@holbertonschool.com>
2020-12-14 02:40:53 +0000

    Fixed code for seg fault.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,51 +1,33 @@
 #include "lists.h"
-
 /**
  * delete_dnodeint_at_index - deletes a node at a specific index in a list.
  * @head: the start of the list.
  * @index: the index of the node that should be deleted.
  * Return: 1 if succeeded, -1 if it failed.
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = (*head), *head2 = (*head), *next_node;
-	unsigned int pos = 1, len = 0;
+	dlistint_t *current = (*head);
+	unsigned int pos = 0;
 
-	if ((*head) == NULL || head == NULL)
+	while (current != NULL)
 	{
-		return (-1);
+		if (pos == index)
+			break;
+		current = current->next;
+		if (current)
+			pos++;
 	}
-
-	while (head2 != NULL)
+	if (current)
 	{
-		head2 = head2->next;
-		len++;
-	}
-
-	if (index == 0)
-	{
-		(*head) = current->next;
+		if (current->next)
+			current->next->prev = current->prev;
+		if (current->prev)
+			current->prev->next = current->next;
+		else
+			*head = current->next;
 		free(current);
 		return (1);
 	}
-	else if (index <= len)
-	{
-		while (pos < index)
-		{
-			current = current->next;
-			pos++;
-		}
-		next_node = current->next;
-
-		current->next = next_node->next;
-
-		free(next_node);
-
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (-1);
 }
