9c8a729b3cdcda417554f420e9a48a784b27b63a
Jeff Martinez <2408@holbertonschool.com>
2020-12-22 23:10:09 +0000

    betty checked.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - that prints every minute of the day of Jack Bauer, starting from
- *               00:00 to 23:59.
+ * jack_bauer - that prints every minute of the day of Jack Bauer,
+ *              starting from 00:00 to 23:59.
  *
  * Return: nothing;
  */
