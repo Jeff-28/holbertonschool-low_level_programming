f0749dbdac88e33d09b9489ecd6cfab09d780d25
Jeff Martinez <2408@holbertonschool.com>
2020-12-23 18:35:44 +0000

    Adding file.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - computes and prints the sum of all the multiples
+ *        of 3 or 5 below 1024 (excluded)
+ * Return: value 0
+ */
+int main(void)
+{
+	int i = 0, res = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+		i++;
+	}
+	printf("%d\n", res);
+	return (0);
+}
