df380c7654cbc11358a1407cf667f5a6ebdd206d
Jeff Martinez <2408@holbertonschool.com>
2020-12-28 18:08:20 +0000

    Fixed code.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,29 +6,19 @@
  */
 int _atoi(char *s)
 {
-	int num = 0, sign = 1, j, i = 0, len = 0;
+	int num = 0, sign = 1, i = 0, j;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-			break;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	for (j = i; j < len; j++)
+	j = i;
+	while (s[j] >= '0' && s[j] <= '9')
 	{
-		if (!(s[j] <= '9' && s[j] >= '0'))
-			break;
+		num = num * 10 + sign * (s[j] - '0');
+		j++;
 	}
-	if (s[i - 1] == '-')
-		sign = -1;
-	len = j - i;
-	while (len >= 1)
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
-		len--;
-	}
-	return (sign * num);
+	return (num);
 }
