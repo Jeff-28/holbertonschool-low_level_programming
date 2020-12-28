6d4568b9c2a899a15f845fa353697ba8450bbdcc
Jeff Martinez <2408@holbertonschool.com>
2020-12-28 19:08:40 +0000

    Fixed code.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,21 @@ int main(void)
 
 	while (num <= 100)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
+		if ((num % 3 != 0) && (num % 5 != 0))
 		{
-			printf("FizzBuzz ");
+			printf("%d", num);
 		}
-		else if ((num % 3) == 0)
+		if ((num % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if ((num % 5) == 0)
+		if ((num % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-		else
+		if (num != 100)
 		{
-			printf("%d ", num);
+			printf(" ");
 		}
 		num++;
 	}
