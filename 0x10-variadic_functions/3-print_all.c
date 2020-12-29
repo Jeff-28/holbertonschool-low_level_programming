d10ad5a094e9853d6fac4a1149ffc7a10578e49f
Jeff Martinez <2408@holbertonschool.com>
2020-12-29 01:33:49 +0000

    Fixed code.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,9 +1,7 @@
 #include "variadic_functions.h"
-
 /**
  * print_char - prints a character.
  * @arguments: list of arguments.
- * Return: nothing.
  */
 void print_char(va_list arguments)
 {
@@ -12,48 +10,48 @@ void print_char(va_list arguments)
 /**
  * print_int - prints an integer.
  * @arguments: list of arguments.
- * Return: nothing.
  */
 void print_int(va_list arguments)
 {
-	printf("%i", va_arg(arguments, int));
+	printf("%d", va_arg(arguments, int));
 }
-
 /**
  * print_str - prints a string.
  * @arguments: list of arguments.
- * Return: nothing.
  */
 void print_str(va_list arguments)
 {
 	char *temp = va_arg(arguments, char *);
 
 	if (temp == NULL)
-	{
 		temp = "(nil)";
-	}
 	printf("%s", temp);
 }
-
+/**
+ * print_f - prints a float
+ * @f: float number
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
 /**
  * print_all - prints anything.
  * @format: list of types of arguments passed to the function.
- * Return: nothing.
  */
 void print_all(const char * const format, ...)
 {
-	int i, j;
+	int i = 0, j;
 	char *separator = "";
 	va_list arguments;
 	printer_t printer_func[] = {
 		{'c', print_char},
 		{'i', print_int},
 		{'s', print_str},
-		{0, NULL}};
+		{"f", print_f},
+		{NULL, NULL}};
 
 	va_start(arguments, format);
-
-	i = 0;
 	while (format && format[i])
 	{
 		j = 0;
@@ -70,6 +68,5 @@ void print_all(const char * const format, ...)
 		i++;
 	}
 	printf("\n");
-
 	va_end(arguments);
 }
