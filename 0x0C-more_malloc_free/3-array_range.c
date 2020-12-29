f38ae551063bf0ef51d20e3ef844fcf9ff65980d
Jeff Martinez <2408@holbertonschool.com>
2020-12-29 01:13:43 +0000

    Fixed code.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,45 +1,27 @@
 #include <stdlib.h>
 #include "holberton.h"
-
 /**
  * array_range - creates an array of integers.
  * @min: should denote the lowest parameter of values.
  * @max: should denote the highest parameter of values.
  * Return: pointer to new array.
  */
-
 int *array_range(int min, int max)
 {
+	int *mem;
+	int i = 0, dist;
 
-int *mem, *array;
-int dist = min;
-
-if (min > max)
-{
-return (NULL);
-}
-
-while (dist < max)
-{
-dist++;
-}
-
-mem = malloc(sizeof(*mem) * dist);
-
-if (mem == NULL)
-{
-free(mem);
-return (NULL);
-}
-
-array = mem;
-
-while (*array < max)
-{
-*array = min;
-array++;
-min++;
-}
-
-return (mem);
+	if (min > max)
+		return (NULL);
+	dist = max - min + 1;
+	mem = malloc(sizeof(int) * dist);
+	if (mem == NULL)
+		return (NULL);
+	while (i < dist)
+	{
+		mem[i] = min;
+		i++;
+		min++;
+	}
+	return (mem);
 }
