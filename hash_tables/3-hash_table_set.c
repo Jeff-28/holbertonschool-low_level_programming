9a12cfc9cbca830f1133fa5e9d0c5889a2c6e696
Jeff Martinez <2408@holbertonschool.com>
2022-08-14 22:28:10 +0000

    Delete the condition to check if the pointer at @idx is NULL

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,28 +6,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *new;
 
-	if (strlen(key))
+	if (strlen(key) == 0)
 		return 0;
 
 	idx = key_index((const unsigned char *)key, 1024);
-	if (ht->array[idx] == NULL)
-	{
-		ht->array[idx] = malloc(sizeof(hash_node_t));
-		if(ht->array[idx] == NULL)
-			return 0;
-	}
-	else
-	{
-		new = malloc(sizeof(hash_node_t));
-		if(new == NULL)
-			return 0;
+	new = malloc(sizeof(hash_node_t));
+	if(new == NULL)
+		return 0;
 
-		new->next = ht->array[idx];
-		ht->array[idx] = new;
-	}
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
 
 	ht->array[idx]->key = strdup(key);
 	ht->array[idx]->value = strdup(value);
 
-	return 1;
+	return idx;
 }
