56e8297b68322537550e060e385996450353579e
Jeff Martinez <2408@holbertonschool.com>
2022-08-16 18:53:28 +0000

    Validate input variables

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -5,9 +5,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
 
+	if (!ht || !key)
+		return NULL;
 	idx = key_index((const unsigned char *)key, 1024);
 
-	if (ht->array[idx])
+	if (ht->array[idx] && (strcmp(ht->array[idx]->key, key) == 0))
 	{
 		return ht->array[idx]->value;
 	}
