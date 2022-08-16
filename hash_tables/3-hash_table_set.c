700e07146b543348833becc90e7e4cd97a26afa0
Jeff Martinez <2408@holbertonschool.com>
2022-08-16 18:46:48 +0000

    Fix logical operator for first condition

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *new;
 
-	if (!ht && strlen(key) == 0)
+	if (!ht || strlen(key) == 0)
 		return 0;
 
 	idx = key_index((const unsigned char *)key, ht->size);
