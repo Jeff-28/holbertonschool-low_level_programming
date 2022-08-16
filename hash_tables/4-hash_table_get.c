bc74b1d9716e0385cc869e7088c6001602bf3f1a
Jeff Martinez <2408@holbertonschool.com>
2022-08-16 19:17:59 +0000

    Check for possible collisions

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,6 +4,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
+	hash_node_t *temp;
 
 	if (!ht || !key)
 		return NULL;
@@ -12,6 +13,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht->array[idx] && (strcmp(ht->array[idx]->key, key) == 0))
 	{
 		return ht->array[idx]->value;
+	}
+	temp = ht->array[idx];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return temp->value;
+		}
+		temp = temp->next;
 	}
 	return NULL;
 }
