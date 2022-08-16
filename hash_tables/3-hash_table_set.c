2cc728f1a04be24194652be4f05fd44370f91191
Jeff Martinez <2408@holbertonschool.com>
2022-08-16 18:22:28 +0000

    Return NULL if malloc fails

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,9 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new = malloc(sizeof(hash_node_t));
 	if(new == NULL)
-		return 0;
-
-	
+		return NULL;
 
 	if (!ht->array[idx])
 	{
